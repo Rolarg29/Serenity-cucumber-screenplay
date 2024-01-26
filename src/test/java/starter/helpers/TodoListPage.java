c7fff4a5d3b934442e5baff681dedf6704c8af4d
Rolando Ramirez <rolurmzguerra@gmail.com>
2024-01-26 12:59:17 -0600

    target name correction

--- src/test/java/starter/helpers/TodoListPage.java
+++ src/test/java/starter/helpers/TodoListPage.java
@@ -7,7 +7,7 @@ import net.serenitybdd.screenplay.targets.Target;
 //TO DO PAGE OBJECT
 @DefaultUrl("https://todomvc.com/examples/react/dist/")
 public class TodoListPage extends PageObject {
-    public static Target ITEM_NAME_FIELD = Target.the("itemp name field").locatedBy(".new-todo");
+    public static Target ITEM_NAME_FIELD = Target.the("item name field").locatedBy(".new-todo");
 
     public static Target ITEMS_LIST = Target.the(" item list").locatedBy(".todo-list li");
 }
